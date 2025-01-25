a71a213dc4e08b26a300f817e60e32c486b12449
zhufucdev <zhufuzhufu1@gmail.com>
2025-01-25 21:30:13 +0800

    :boom: dynamic size

--- src/ds/graph.rs
+++ src/ds/graph.rs
@@ -3,8 +3,8 @@ use std::fmt;
 use std::fmt::Formatter;
 
 pub(crate) struct NdGraph {
-    len: usize,
-    capacity: usize,
+    len: u32,
+    capacity: u32,
     adjacent_matrix: Vec<Vec<f32>>,
 }
 
@@ -25,7 +25,7 @@ impl fmt::Display for Boundary {
 
 #[derive(Debug, PartialEq)]
 pub(crate) enum Error {
-    ExceedBoundary(Boundary, usize, usize),
+    ExceedBoundary(Boundary, u32, u32),
 }
 
 impl fmt::Display for Error {
@@ -40,97 +40,120 @@ impl fmt::Display for Error {
 }
 
 impl NdGraph {
-    pub(crate) fn new(capacity: usize) -> NdGraph {
+    pub(crate) fn new() -> NdGraph {
         NdGraph {
             len: 0,
-            capacity,
-            adjacent_matrix: Vec::from_iter(
-                (0..capacity).map(|row| Vec::from_iter((0..=row).map(|_| f32::INFINITY))),
-            ),
+            capacity: 0,
+            adjacent_matrix: Vec::new(),
         }
     }
 
-    pub(crate) fn from_adj_list(capacity: usize, adj_list: Vec<(usize, usize, f32)>) -> NdGraph {
-        let adj_mat = Vec::from_iter((0..capacity).map(|row| {
-            Vec::from_iter((0..=row).map(|col| {
-                match adj_list
-                    .iter()
-                    .find(|(a, b, _)| *a == row && *b == col || *a == col && *b == row)
-                {
-                    None => f32::INFINITY,
-                    Some((_, _, d)) => *d,
-                }
-            }))
-        }));
+    pub(crate) fn with_capacity(capacity: u32) -> NdGraph {
+        NdGraph {
+            len: 0,
+            capacity,
+            adjacent_matrix: (0..capacity)
+                .map(|row| (0..=row).map(|_| f32::INFINITY).collect())
+                .collect(),
+        }
+    }
+
+    pub(crate) fn from_adj_list(adj_list: Vec<(u32, u32, f32)>) -> NdGraph {
         let len = *adj_list
             .iter()
             .flat_map(|(a, b, _)| [a, b])
             .max()
-            .unwrap_or(&0usize);
+            .unwrap_or(&0u32);
+
+        let adj_mat = (0..len)
+            .map(|row| {
+                (0..=row)
+                    .map(|col| {
+                        match adj_list
+                    .iter()
+                    .find(|(a, b, _)| *a == row && *b == col || *a == col && *b == row)
+                    // TODO: optimize this O(n^2) search by sorting in advance
+                {
+                    None => f32::INFINITY,
+                    Some((_, _, d)) => *d,
+                }
+                    })
+                    .collect()
+            })
+            .collect();
 
         NdGraph {
             len,
-            capacity,
+            capacity: len,
             adjacent_matrix: adj_mat,
         }
     }
 
-    pub(crate) fn get_neighbors(&self, query_node: usize) -> Vec<(usize, f32)> {
-        if query_node >= self.len {
+    pub(crate) fn get_neighbors(&self, query_node: u32) -> Vec<(u32, f32)> {
+        if query_node >= self.len() {
             return vec![];
         }
         Vec::from_iter(
-            (0..self.len)
-                .map(|n| (n, self.adjacent_matrix[query_node][n]))
-                .filter(|n| self.adjacent_matrix[query_node][n.0] < f32::INFINITY),
+            (0..self.len())
+                .map(|n| (n, self.adjacent_matrix[query_node as usize][n as usize]))
+                .filter(|n| self.adjacent_matrix[query_node as usize][n.0 as usize] < f32::INFINITY),
         )
     }
 
-    pub(crate) fn insert(&mut self, count: usize) -> Result<usize, Error> {
-        if self.capacity < self.len + count {
+    pub(crate) fn insert(&mut self, count: u32) -> Result<u32, Error> {
+        if self.capacity < self.len() + count {
             return Err(Error::ExceedBoundary(
                 Boundary::Capacity,
                 self.capacity + 1,
-                self.len,
+                self.len(),
             ));
         }
 
+        let old_len = self.len();
+        for i in 0..count {
+            self.adjacent_matrix
+                .push(Vec::from_iter((0..i + old_len).map(|_| f32::INFINITY)))
+        }
         self.len += count;
-        Ok(self.len - 1)
+        Ok(self.len() - 1)
     }
 
-    pub(crate) fn insert_one(&mut self) -> Result<usize, Error> {
+    pub(crate) fn insert_one(&mut self) -> Result<u32, Error> {
         self.insert(1)
     }
 
-    pub(crate) fn connect(&mut self, a: usize, b: usize, distance: f32) -> Result<(), Error> {
-        if a >= self.len || b >= self.len {
+    pub(crate) fn connect(&mut self, a: u32, b: u32, distance: f32) -> Result<(), Error> {
+        if a >= self.len() || b >= self.len() {
             Err(Error::ExceedBoundary(
                 Boundary::Index,
                 max(a, b) + 1,
-                self.len,
+                self.len(),
             ))
         } else {
             let (a, b) = if a > b { (a, b) } else { (b, a) };
-            self.adjacent_matrix[a][b] = distance;
+            self.adjacent_matrix[a as usize][b as usize] = distance;
             Ok(())
         }
     }
 
-    pub(crate) fn len(&self) -> usize {
+    pub(crate) fn len(&self) -> u32 {
         self.len
     }
 
-    pub(crate) fn capacity(&self) -> usize {
+    pub(crate) fn capacity(&self) -> u32 {
         self.capacity
     }
 
-    pub(crate) fn distance_between(&self, a: usize, b: usize) -> Result<Option<f32>, Error> {
-        if a >= self.len || b >= self.len {
-            Err(Error::ExceedBoundary(Boundary::Index, max(a, b) + 1, self.len))
+    pub(crate) fn distance_between(&self, a: u32, b: u32) -> Result<Option<f32>, Error> {
+        if a >= self.len() || b >= self.len() {
+            Err(Error::ExceedBoundary(
+                Boundary::Index,
+                max(a, b) + 1,
+                self.len(),
+            ))
         } else {
             let (a, b) = if a > b { (a, b) } else { (b, a) };
-            let dis = self.adjacent_matrix[a][b];
+            let dis = self.adjacent_matrix[a as usize][b as usize];
             Ok(if dis < f32::INFINITY { Some(dis) } else { None })
         }
     }
@@ -144,26 +167,26 @@ mod test {
     #[test]
     fn constructors_work() {
         for size in 1..=14 {
-            _ = NdGraph::new(size);
+            _ = NdGraph::with_capacity(size);
         }
     }
 
     #[test]
     fn insertion_works() {
-        let mut graph = NdGraph::new(10);
+        let mut graph = NdGraph::with_capacity(10);
 
         assert_eq!(graph.insert_one().unwrap(), 0);
         // out of bound
         graph.insert(9).unwrap();
         assert_eq!(
             graph.insert_one(),
-            Err(Error::ExceedBoundary(Boundary::Capacity, 11usize, 10usize))
+            Err(Error::ExceedBoundary(Boundary::Capacity, 11u32, 10u32))
         );
     }
 
     #[test]
     fn connectivity_works() {
-        let mut graph = NdGraph::new(10);
+        let mut graph = NdGraph::with_capacity(10);
         graph.insert(graph.capacity()).unwrap();
         graph.connect(0, 9, E).unwrap();
         assert_eq!(graph.distance_between(0, 9).unwrap().unwrap(), E);
